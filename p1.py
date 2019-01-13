03f06cff35816e023ad252d7eaee1ba009e6b919
maynasser <44294551+maynasser@users.noreply.github.com>
2019-01-13 22:02:46 +0300

    Update p1.py
    - Add  shebang line correctly.
    - Add Error handling in db_connection().
    - Using .format() method for printing output.
    - Change 100 to 100.0. on third query to get float number instead of integer.
    - Add ```python  if __name__ == "__main__": ``` to makes sure that the code in that section is only run when this program is executed directly, and not when it is imported as a module.

--- p1.py
+++ p1.py
@@ -1,20 +1,23 @@
-import psycopg2
-'#!/usr/bin/env python3'
-'#logs analysis project'
+# !/usr/bin/env python3
 
-'#connecting to db & run query'
+import psycopg2
+# connecting to db & run query'
 
 
 def db_connection(query):
-    db = psycopg2.connect(database="news")
-    c = db.cursor()
-    c.execute(query)
-    results = c.fetchall()
-    db.close()
-    return results
+    try:
+        db = psycopg2.connect(database="news")
+        c = db.cursor()
+        c.execute(query)
+        results = c.fetchall()
+        return results
+    except psycopg2.DatabaseError as e:
+        print('Sorry, there is an error : %s' % e)
+    finally:
+        db.close()
 
 
-'#What are the most popular three articles of all time?'
+# What are the most popular three articles of all time?
 
 
 def q1_answer():
@@ -24,13 +27,14 @@ def q1_answer():
     popular_articles = db_connection(a1)
     print('the most popular three articles of all time are: ')
     count = 1
-    for i in popular_articles:
-        print('(' + str(count) + ') ' + i[0] + ' -- ' + str(i[1]) + " views")
+    for (Title, Views) in popular_articles:
+        print("({})  {} -- {} views.".format(count, Title, Views))
         count += 1
+    print("-" * 70)
     pass
 
 
-'# Who are the most popular article authors of all time? #'
+# Who are the most popular article authors of all time?
 
 
 def q2_answer():
@@ -38,42 +42,48 @@ def q2_answer():
     log c where b.slug = substring(c.path, 10) and a.id = b.author
     group by name order by v desc;"""
     popular_authors = db_connection(a2)
-    print("the most popular article authors of all time is:")
+    print("the most popular article authors of all time are:")
     count = 1
-    for i in popular_authors:
-        print('(' + str(count) + ') ' + i[0] + ' with ' + str(i[1]) + " views")
+    for (name, v) in popular_authors:
+        print("({})  {} with {} views.".format(count, name, v))
         count += 1
+    print("-" * 70)
     pass
 
 
-'# On which days did more than 1% of requests lead to errors? #'
+# On which days did more than 1% of requests lead to errors?
 
 
 def q3_answer():
     a3 = """
-    select allReqeusts.date,
-    round( ((errs.err404 *100 ) / allReqeusts.req), 2) as errPercent
+    select allReqeusts.date as d,
+    round( ((errs.err404 *100.0 ) / allReqeusts.req), 2) as errPercent
     from (select to_char(date_trunc('day', time), 'Month dd,  yyyy') as date,
     count(*) as err404
     from log where status like '404%' group by date) as errs
     join (select to_char(date_trunc('day', time), 'Month dd,  yyyy') as date,
     count(*) as req from log group by date) as allReqeusts
     on errs.date = allReqeusts.date
-    where ((round( ((errs.err404 * 100) / allReqeusts.req), 2)) > 1)
+    where ((round( ((errs.err404 * 100.0) / allReqeusts.req), 2)) > 1)
     order by errPercent  desc;
     """
     errors = db_connection(a3)
     print("Days did more than '1%' of requests lead to errors are: ")
-    for i in errors:
-        print(str(i[0]) + " -- " + str(i[1]) + " %" + " errors")
+    for (d, errPercent) in errors:
+        print("{} -- {} % errors".format(d, errPercent))
     pass
 
 
-q1_answer()
-q2_answer()
-q3_answer()
+if __name__ == '__main__':
+    q1_answer()
+    q2_answer()
+    q3_answer()
+
 
 """Sources and references:
+https://www.digitalocean.com/community/tutorials/how-to-use-string-formatters-in-python-3
+https://www.programcreek.com/python/example/66935/psycopg2.DatabaseError
+https://stackoverflow.com/questions/419163/what-does-if-name-main-do
 https://www.w3schools.com/
 https://www.postgresql.org/docs/9.4/app-psql.html
 https://github.com/michellejl/log_analysis/blob/master/loganalysisdb.py
